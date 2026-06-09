76ea19b78106fbf30d313a6ddae96c1299257efd
Leverage <leveragex502@gmail.com>
2026-06-09 18:24:36 +0530

    Update backend

--- Models/db.js
+++ Models/db.js
@@ -1,10 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const mongo_url = process.env.MONGO_CONN;
 
-mongoose.connect(mongo_url)
-    .then(() => {
-        console.log('MongoDB Connected...');
-    }).catch((err) => {
-        console.log('MongoDB Connection Error: ', err);
-    })
+console.log("Attempting MongoDB connection...");
+
+mongoose
+  .connect(mongo_url)
+  .then(() => {
+    console.log("✅ MongoDB Connected");
+  })
+  .catch((err) => {
+    console.error("❌ MongoDB Connection Error");
+    console.error(err);
+  });
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose connected");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("Mongoose error:", err);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongoose disconnected");
+});
